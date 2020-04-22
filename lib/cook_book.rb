221f0a68583b766c8c0736adb6c83b1681edbd25
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-04-22 10:44:17 -0600

    Add ingredients method and test to cookbook

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -7,4 +7,12 @@ attr_reader :recipes
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    ingredients = []
+    @recipes.each do |recipe|
+      ingredients << recipe.ingredients.map {|ingredient| ingredient.name }
+    end
+    ingredients.flatten.uniq
+  end
 end
--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -8,9 +8,18 @@ class CookBookTest < Minitest::Test
   def setup
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @pantry = Pantry.new
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+
     @cookbook = CookBook.new
   end
 
@@ -28,6 +37,13 @@ class CookBookTest < Minitest::Test
     @cookbook.add_recipe(@recipe2)
 
     assert_equal [@recipe1, @recipe2], @cookbook.recipes
+  end
+
+  def test_it_can_get_ingredients
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    expected = ["Cheese", "Macaroni", "Ground Beef", "Bun"]
+    assert_equal expected, @cookbook.ingredients
   end
 end
 

a7a131f1ef49124b141ead162d3d33b5fbefecbd
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-04-22 09:27:29 -0600

    Add pantry class and test

---
+++ test/pantry_test.rb
@@ -0,0 +1,37 @@
+require "minitest/autorun"
+require "./lib/ingredient"
+require "./lib/pantry"
+
+class PantryTest < Minitest::Test
+  def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @pantry = Pantry.new
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, @pantry
+  end
+end
+
+# pry(main)> pantry = Pantry.new
+# # => #<Pantry:0x007fd8858863b8...>
+#
+# pry(main)> pantry.stock
+# # => {}
+#
+# pry(main)> pantry.stock_check(ingredient1)
+# # => 0
+#
+# pry(main)> pantry.restock(ingredient1, 5)
+#
+# pry(main)> pantry.restock(ingredient1, 10)
+#
+# pry(main)> pantry.stock_check(ingredient1)
+# # => 15
+#
+# pry(main)> pantry.restock(ingredient2, 7)
+#
+# pry(main)> pantry.stock_check(ingredient2)
+# # => 7
+# ```

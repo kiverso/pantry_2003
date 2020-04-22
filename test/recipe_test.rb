c2cdf91f40d16d742673156fcf3f3159fadf712d
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-04-22 10:33:16 -0600

    Add total_calories method and test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -5,10 +5,13 @@ require "./lib/recipe"
 
 class RecipeTest < Minitest::Test
   def setup
-    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @pantry = Pantry.new
     @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
   end
 
   def test_it_exists
@@ -37,5 +40,17 @@ class RecipeTest < Minitest::Test
 
     @recipe1.add_ingredient(@ingredient2, 8)
     assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients
+  end
+
+  def test_it_can_get_total_calories
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+
+    assert_equal 440, @recipe1.total_calories
+    assert_equal 675, @recipe2.total_calories
   end
 end
